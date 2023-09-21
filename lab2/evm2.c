8acf16289b579995011f0251c5a0c718063dbbd9
Загнеев Дмитрий <143857175+zagdm@users.noreply.github.com>
2023-09-21 23:21:34 +0700

    Add files via upload (#2)

---
+++ lab2/evm2.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+
+#define input_error 1
+#define step 70000000
+#define init_quant 600000000
+
+double e_sin(double x) {
+	return exp(x) * sin(x);
+}
+
+double calc_integral(double f(double), int N, double a, double b) {
+	double m_riem_sum = 0;
+	const double dx = (double)(b - a) / N;
+	for (int i = 0; i < N; i++) m_riem_sum += f(a + i * dx) + f(a + (i + 1) * dx);
+	return m_riem_sum * dx / 2;
+}
+
+int main(int argc, char* argv[]) {
+	if (argc < 2) return input_error;
+	double integral = calc_integral(e_sin, init_quant + step * atoi(argv[1]), 0, M_PI);
+	printf("%20.19f", integral);
+	return 0;
+}
